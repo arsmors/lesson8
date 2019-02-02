b5884dbdfc93a772039ea326b4c949973257f358
Arsens <arsens.morins@chi.lv>
2019-02-02 10:50:04 +0200

    local notification added

--- app/src/main/java/com/example/den/lesson8/MainActivity.java
+++ app/src/main/java/com/example/den/lesson8/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.den.lesson8;
 
 import android.app.Activity;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.NotificationCompat;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -37,6 +42,8 @@ public class MainActivity extends Activity implements PhotoItemsPresenterCallbac
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         showImgService(ImgServices.GIPHY);
+
+        showNotificationAfterDelay(getBasicNotification());
     }
 
     private void showImgService(ImgServices service) {
@@ -59,6 +66,45 @@ public class MainActivity extends Activity implements PhotoItemsPresenterCallbac
                 })
         );
     }
+
+
+    private NotificationCompat.Builder getBasicNotification() {
+
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this, "default")
+                .setSmallIcon(R.drawable.white_notification_icon)
+                .setContentTitle("This is my test notification")
+                .setContentText("Notification visible when app starts")
+//                .setStyle(new NotificationCompat.BigTextStyle()
+//                        .bigText("Much longer text that cannot fit one line..."))
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        return mBuilder;
+    }
+
+    private void showNotificationAfterDelay(final NotificationCompat.Builder notification) {
+
+        final NotificationManager mNotificationManager =
+                (NotificationManager) getSystemService(this.NOTIFICATION_SERVICE);
+
+        if (Build.VERSION.SDK_INT >= 26) {
+            String id = "my_channel_01";
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel mChannel = new NotificationChannel(id, "Channel name",importance);
+            mChannel.enableLights(true);
+            mNotificationManager.createNotificationChannel(mChannel);
+            notification.setChannelId(id);
+        }
+
+        final Handler handler = new Handler();
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mNotificationManager.notify(0, notification.build());
+            }
+        }, 5000);
+    }
+
 
     @Override
     public void onItemSelected(PhotoItem item) {
